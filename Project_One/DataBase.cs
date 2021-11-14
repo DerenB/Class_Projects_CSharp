70a7ce02d23295c40bc9786e80cbf0be0dce0bab
Deren B <dbozerprogramming@gmail.com>
2021-11-13 21:48:54 -0500

    Creating Database class and data set

---
+++ Project_One/DataBase.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_One {
+    public class DataBase {
+        private DataBaseRecord[] data;
+        private int nextDBRecord;
+        Index firstIndex, lastIndex, idIndex;
+
+    }
+}
